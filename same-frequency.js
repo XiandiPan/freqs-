d68d394f85b7a6dd15725ab8ad6af20912503d84
Xiandi Pan <cindypan1807@gmail.com>
2023-04-07 19:44:53 -0400

    p2

--- same-frequency.js
+++ same-frequency.js
@@ -3,8 +3,47 @@
 //create useFrequencyCount function
 //create number frequencies calling useFrequencyCount on split numbers-turned-string
 //iterate through each key in first number frequency
-  //check if key is in second num freqency, if not => false
-  //check if it equals second num frequency, if not => false
+//check if key is in second num frequency, if not => false
+//check if it equals second num frequency, if not => false
 //return true
-function sameFrequency() {
+
+/** input: two integers. output: boolean
+*
+*sameFrequency(123, 321);   // true
+*sameFrequency(34, 14);     // false
+*sameFrequency(22, 22);     // true
+*sameFrequency(22, 222);    // false
+
+
+*/
+function getFreqCounter(items) {
+  const frq = {};
+
+  for (const item of items) {
+    const current = frq[item] || 0;
+    frq[item] = current + 1;
+  }
+  return frq;
+}
+
+function sameFrequency(num1, num2) {
+  const numStr1 = num1.toString();
+  const numStr2 = num2.toString();
+
+  if (numStr1.length !== numStr2.length) return false;
+
+  const frq1 = getFreqCounter(numStr1);
+  const frq2 = getFreqCounter(numStr2);
+
+  for (let key in frq1){
+    if (!(key in frq2)){
+      return false;
+    }
+
+    if (frq1[key] !== frq2[key]){
+      return false;
+    }
+  }
+  return true;
+
 }
--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,7 @@
+"use strict"
+
+//declare a function pass in a array of non-zero numbers
+
 // add whatever parameters you deem necessary & write docstring
 function separatePositive() {
 }
