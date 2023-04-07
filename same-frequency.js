c2adaf4f805f9851855c8c53db5c8766d7181ba5
John Chong <johnchong.99.17@gmail.com>
2023-04-07 18:22:37 -0500

    work same-frequency

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,10 @@
-// add whatever parameters you deem necessary & write docstring
+"use strict";
+
+//create useFrequencyCount function
+//create number frequencies calling useFrequencyCount on split numbers-turned-string
+//iterate through each key in first number frequency
+  //check if key is in second num freqency, if not => false
+  //check if it equals second num frequency, if not => false
+//return true
 function sameFrequency() {
 }
